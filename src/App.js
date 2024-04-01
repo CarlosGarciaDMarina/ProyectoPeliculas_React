5a432d037905df1077fe5a1ea32db83331f2925e
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-04-01 14:18:23 +0200

    Maquetacion del proyecto - 50%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="layout">
+
+      {/* Cabecera */}
+      <header className='header'>
+        <div className='logo'>
+          <div className='play'></div>
+        </div>
+        <h1>Mis peliculas</h1>
       </header>
+
+      {/* Barra de navegación */}
+      <nav className='nav'>
+        <ul>
+          <li>
+            <a href='/#'>Inicio</a>
+          </li>
+          <li>
+            <a href='/#'>Peliculas</a>
+          </li>
+          <li>
+            <a href='/#'>Blog</a>
+          </li>
+          <li>
+            <a href='/#'>Contacto</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Contenido principal */}
+      <section className='content'>
+        {/* Peliculas */}
+        <article className='peli-item'>
+          <h3 className='title'>Desarrollo Web</h3>
+          <p className='description'>carlosgarciaweb.es</p>
+
+          <button className='edit'>Editar</button>
+          <button className='delete'>Borrar</button>
+        </article>
+
+        <article className='peli-item'>
+          <h3 className='title'>Desarrollo Web</h3>
+          <p className='description'>carlosgarciaweb.es</p>
+
+          <button className='edit'>Editar</button>
+          <button className='delete'>Borrar</button>
+        </article>   
+
+        <article className='peli-item'>
+          <h3 className='title'>Desarrollo Web</h3>
+          <p className='description'>carlosgarciaweb.es</p>
+
+          <button className='edit'>Editar</button>
+          <button className='delete'>Borrar</button>
+        </article>
+
+        <article className='peli-item'>
+          <h3 className='title'>Desarrollo Web</h3>
+          <p className='description'>carlosgarciaweb.es</p>
+
+          <button className='edit'>Editar</button>
+          <button className='delete'>Borrar</button>
+        </article>
+
+      </section>
+
+      {/* Barra lateral */}
+      <aside className='lateral'>
+        <div className='search'>
+          <h3 className='title'>Buscador</h3>
+          <form>
+            <input type='text' />
+            <button>Buscar</button>
+          </form>
+        </div>
+
+        <div className='add'>
+          <h3 className='title'>Añadir pelicula</h3>
+          <form>
+            <input type='text' placeholder='Título' />
+            <textarea placeholder='Descripción' />
+            <input type='submit' value="Guardar" />
+          </form>
+        </div>
+      </aside>
+
+      {/* Pie de página */}
+      <footer className='footer'>
+        Carlos Garcia de Marina &copy; 
+      </footer>
+
     </div>
   );
 }
